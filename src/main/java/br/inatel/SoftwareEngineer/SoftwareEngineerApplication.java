2bfb4e3c7382f079826521d54c8b39fdf7156ca5
Dimitri <dimitri.s.amado@gmail.com>
2026-03-23 20:08:59 -0300

    config: Java Spring basic structure

---
+++ src/main/java/br/inatel/SoftwareEngineer/SoftwareEngineerApplication.java
@@ -0,0 +1,13 @@
+package br.inatel.SoftwareEngineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareEngineerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/inatel/SoftwareEngineer/SoftwareEngineerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.inatel.SoftwareEngineer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
