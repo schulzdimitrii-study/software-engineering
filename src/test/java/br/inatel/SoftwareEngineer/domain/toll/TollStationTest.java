15dbbc051fe35c4eb61da762da2cf77a12b9df43
LuizGustavo00 <lgcosta1@hotmail.com>
2026-03-28 09:05:03 -0300

    feat: Add tollStation class and tollStatioTest

---
+++ src/test/java/br/inatel/SoftwareEngineer/domain/toll/TollStationTest.java
@@ -0,0 +1,50 @@
+package br.inatel.SoftwareEngineer.domain.toll;
+
+import br.inatel.SoftwareEngineer.domain.vehicles.Vehicle;
+import br.inatel.SoftwareEngineer.domain.vehicles.VehicleType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TollStationTest {
+
+    private TollStation tollStation;
+
+    @BeforeEach
+    void setUp() {
+        tollStation = new TollStation("Pedágio Principal", new BigDecimal("5.00"));
+    }
+
+    @Test
+    void calculateSixAxlesTruck() {
+        Vehicle truck = new Vehicle("ABC-1234", VehicleType.TRUCK, 6, false);
+        
+        BigDecimal result = tollStation.calculateToll(truck, LocalTime.of(10, 0));
+
+        assertEquals(new BigDecimal("30.00"), result);
+    }
+
+    @Test
+    void peakHour() {
+        Vehicle car = new Vehicle("XYZ-9876", VehicleType.CAR, 2, false);
+
+        BigDecimal normalResult = tollStation.calculateToll(car, LocalTime.of(10, 0));
+        assertEquals(new BigDecimal("10.00"), normalResult);
+
+        BigDecimal peakResult = tollStation.calculateToll(car, LocalTime.of(8, 0));
+        assertEquals(new BigDecimal("12.00"), peakResult);
+    }
+
+    @Test
+    void emergencyVehicles() {
+        Vehicle ambulance = new Vehicle("SOS-1920", VehicleType.CAR, 2, true);
+        
+        BigDecimal result = tollStation.calculateToll(ambulance, LocalTime.of(8, 0));
+
+        assertEquals(new BigDecimal("0.00"), result);
+    }
+}
