c2ac498b63afdafc1d8bae38b639119b5def1358
Dimitri <dimitri.s.amado@gmail.com>
2026-04-10 16:03:52 -0300

    fix: replace VehicleRepository primary key type from Integer to UUID

--- src/main/java/br/inatel/SoftwareEngineer/repository/VehicleRepository.java
+++ src/main/java/br/inatel/SoftwareEngineer/repository/VehicleRepository.java
@@ -1,10 +1,11 @@
 package br.inatel.SoftwareEngineer.repository;
 
 import br.inatel.SoftwareEngineer.domain.vehicles.Vehicle;
+
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
 }
-
