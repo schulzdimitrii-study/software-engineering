cc0132341952b181cdcc7fa361f509b8a09c634b
eRosa21 <rosaesthefano@gmail.com>
2026-04-10 22:45:01 -0300

    implemen CRUD endpoints, validation and test suite. PassageControlller containing basic CRUD endopoint. TallStationRepository

--- src/main/java/br/inatel/SoftwareEngineer/repository/TallStationRepository.java
+++ src/main/java/br/inatel/SoftwareEngineer/repository/TallStationRepository.java
@@ -1,7 +1,6 @@
 package br.inatel.SoftwareEngineer.repository;
 
 import br.inatel.SoftwareEngineer.domain.tallStation.TallStation;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
